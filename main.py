bd9a7545d2c12ee456debef281a5715c92fb7304
JagTheFriend <jagthefriend12@gmail.com>
2021-08-20 18:59:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 import random
 
+string = """0¶1¶2¶3¶4¶5¶6¶7¶8¶9¶a¶b¶c¶d¶e¶f¶g¶h¶i¶j¶k¶l¶m¶n¶o¶p¶q¶r¶s¶t¶u¶v¶w¶x¶y¶z¶A¶B¶C¶D¶E¶F¶G¶H¶I¶J¶K¶L¶M¶N¶O¶P¶Q¶R¶S¶T¶U¶V¶W¶X¶Y¶Z¶`¶~¶!¶@¶#¶$¶%¶^¶&¶*¶(¶)¶-¶_¶=¶+¶[¶]¶\¶{¶}¶|¶;¶'¶:¶"¶,¶.¶/¶<¶>¶?¶ """
+keywords = string.split("¶")
 
-class encryption():
-    def __init__(self):
-        string = """0¶1¶2¶3¶4¶5¶6¶7¶8¶9¶a¶b¶c¶d¶e¶f¶g¶h¶i¶j¶k¶l¶m¶n¶o¶p¶q¶r¶s¶t¶u¶v¶w¶x¶y¶z¶A¶B¶C¶D¶E¶F¶G¶H¶I¶J¶K¶L¶M¶N¶O¶P¶Q¶R¶S¶T¶U¶V¶W¶X¶Y¶Z¶`¶~¶!¶@¶#¶$¶%¶^¶&¶*¶(¶)¶-¶_¶=¶+¶[¶]¶\¶{¶}¶|¶;¶'¶:¶"¶,¶.¶/¶<¶>¶?¶ """
-        keywords = string.split("¶")
-        return None
 
-    def generate_key(self):
-        seed = (random.randint(0, 2000000000))
-        random.seed(seed)
-        string = """0¶1¶2¶3¶4¶5¶6¶7¶8¶9¶a¶b¶c¶d¶e¶f¶g¶h¶i¶j¶k¶l¶m¶n¶o¶p¶q¶r¶s¶t¶u¶v¶w¶x¶y¶z¶A¶B¶C¶D¶E¶F¶G¶H¶I¶J¶K¶L¶M¶N¶O¶P¶Q¶R¶S¶T¶U¶V¶W¶X¶Y¶Z¶`¶~¶!¶@¶#¶$¶%¶^¶&¶*¶(¶)¶-¶_¶=¶+¶[¶]¶\¶{¶}¶|¶;¶'¶:¶"¶,¶.¶/¶<¶>¶?¶ """
-        keywords = string.split("¶")
-        encrypted = []
-        for i in range(94):
-            key = str(random.randint(0, 10000000))
-            if key in encrypted:
-                def redo():
-                    key = str(random.randint(0, 10000000))
-                    if key in encrypted:
-                        redo()
-                    else:
-                        encrypted.append(key)
-                redo()
-            else:
-                encrypted.append(key)
-        return [seed, encrypted]
+class Encryption:
 
-    def encrypt(self, encryption_key, text):
-        string = """0¶1¶2¶3¶4¶5¶6¶7¶8¶9¶a¶b¶c¶d¶e¶f¶g¶h¶i¶j¶k¶l¶m¶n¶o¶p¶q¶r¶s¶t¶u¶v¶w¶x¶y¶z¶A¶B¶C¶D¶E¶F¶G¶H¶I¶J¶K¶L¶M¶N¶O¶P¶Q¶R¶S¶T¶U¶V¶W¶X¶Y¶Z¶`¶~¶!¶@¶#¶$¶%¶^¶&¶*¶(¶)¶-¶_¶=¶+¶[¶]¶\¶{¶}¶|¶;¶'¶:¶"¶,¶.¶/¶<¶>¶?¶ """
-        keywords = string.split("¶")
+    def encrypt(self, encryption_key: int, text: str) -> str:
         random.seed(encryption_key)
         key = []
         for i in range(94):
@@ -38,9 +15,7 @@ class encryption():
             text = str(text.replace(keywords[i], key[i]))
         return text
 
-    def decrypt(self, encryption_key, encrypted_text):
-        string = """0¶1¶2¶3¶4¶5¶6¶7¶8¶9¶a¶b¶c¶d¶e¶f¶g¶h¶i¶j¶k¶l¶m¶n¶o¶p¶q¶r¶s¶t¶u¶v¶w¶x¶y¶z¶A¶B¶C¶D¶E¶F¶G¶H¶I¶J¶K¶L¶M¶N¶O¶P¶Q¶R¶S¶T¶U¶V¶W¶X¶Y¶Z¶`¶~¶!¶@¶#¶$¶%¶^¶&¶*¶(¶)¶-¶_¶=¶+¶[¶]¶\¶{¶}¶|¶;¶'¶:¶"¶,¶.¶/¶<¶>¶?¶ """
-        keywords = string.split("¶")
+    def decrypt(self, encryption_key: int, encrypted_text: str) -> str:
         random.seed(encryption_key)
         key = []
         for i in range(94):
@@ -48,3 +23,9 @@ class encryption():
         for i in range(94):
             encrypted_text = encrypted_text.replace(key[i], keywords[i])
         return encrypted_text
+
+
+ekey = Encryption()
+encrypted_text = ekey.encrypt(1347799866, "asdfghjkl")
+print(encrypted_text)
+print(ekey.decrypt(1347799866, encrypted_text))
